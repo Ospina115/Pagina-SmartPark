1275f28b9635a00562e983dc89bf44e64f99d2d5
Ospina115 <158192117+Ospina115@users.noreply.github.com>
2024-05-15 21:44:14 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("negrito").addEventListener("click", gotoUrl);
+
+function gotoUrl() {
+    window.location.assign("https://api.whatsapp.com/send?phone=573152700602");
+}
+
+document.getElementById("blanquito").addEventListener("click", gotoUrl);
+
+function gotoUrl() {
+    window.location.assign("https://i-park.com.co/service/servicios/");
+}
